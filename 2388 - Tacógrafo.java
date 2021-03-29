5dffd0bba0f2843c506e64bfd47ec1ebafc46ae3
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2021-03-28 22:26:45 -0400

    Uri Code

---
+++ 2388 - Tacógrafo.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner; 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        
+        Scanner leia = new Scanner(System.in);
+        
+        int caso, t, v;
+        
+       caso = leia.nextInt();
+       int total =0;
+       for(; caso >0; caso--)
+       {
+           t = leia.nextInt();
+           v = leia.nextInt();
+           
+           total += + (t*v);
+       }
+        
+        System.out.println(""+total);
+        
+ 
+    }
+ 
+}
