3fe98937ec6320cb11ddb4174182558abd931c5d
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2022-02-13 09:17:40 -0400

    URI

---
+++ Uri_1043.java
@@ -0,0 +1,13 @@
+package uri;
+import java.io.IOException;
+import java.util.*;
+
+public class Uri_1043 {
+
+    public static void main(String[] args) throws IOException {
+        
+        
+
+    }
+    
+}
