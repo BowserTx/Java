4cdc728e89a0cefaa6e643cf2005424faa845bf9
Willian Souza <76187694+BowserTx@users.noreply.github.com>
2022-05-09 12:28:33 -0400

    URI

--- Uri_1043.java
+++ Uri_1043.java
@@ -6,6 +6,25 @@ public class Uri_1043 {
 
     public static void main(String[] args) throws IOException {
         
+        try (Scanner read = new Scanner(System.in)) {
+            double x = read.nextDouble();
+            double y = read.nextDouble();
+            double z = read.nextDouble();
+
+
+            if(x+y > z && x+z > y && z+y > x)
+            {
+                System.out.printf("Perimetro = %.1f", x+y+z);
+                System.out.println();
+            }
+            else
+            {
+                System.out.printf("Area = %.1f", ((x+y)*z)/2);
+                System.out.println();
+            }
+        }
+
+
         
 
     }
